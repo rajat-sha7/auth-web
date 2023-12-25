a730c4c6d27568726d004165b3e3dd9d0c048804
RAJATS SHARMA <108745640+rajat-sha7@users.noreply.github.com>
2023-12-25 16:35:41 +0530

    added sign-in api

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import {signup} from "../controllers/auth.controlar.js";
-
+import {signin} from "../controllers/auth.controlar.js";
 const router =express.Router();
 
 router.post("/signup", signup) 
+router.post("/signin", signin)
 
 export default router;
