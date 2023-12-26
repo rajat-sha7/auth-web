c389b19c0c8c69930336a9924914de796bf1dc98
RAJATS SHARMA <108745640+rajat-sha7@users.noreply.github.com>
2023-12-26 20:53:44 +0530

    added redux persiit

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,24 @@
-import {configureStore} from "@reduxjs/toolkit";
-import userReducer from "./user/userSlice.js"
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import userReducer from "./user/userSlice.js";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+const rootReducer = combineReducers({ user: userReducer });
 
-export const store= configureStore({
+const persistConfig = {
+  key: "user",
+  storage,
+  version: 1
+};
 
-    reducer: {user: userReducer},
-    middleware: (getDefaultNiddleware) => getDefaultNiddleware({
-        serializableCheck : false,
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      // serializableCheck: false // Enable this in production for better data integrity
     })
+});
 
-
-})
+export const persistor = persistStore(store);
